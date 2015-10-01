01c019c0d0e6332abae2420abe60ad58fc690ba9
kusera <kuserasera@gmail.com>
2015-10-01 12:08:59 -0400

    Add comments to actions for detail

--- src/js/actions/MapActions.js
+++ src/js/actions/MapActions.js
@@ -1,11 +1,19 @@
 import { SWITCH_FACTION, TOGGLE_FILTER, TOGGLE_GROUP } from '../constants/MapConstants';
 
+/*
+  Associated with the FactionSwitch component
+  Changes whether the map should display from the perspective of radiant or dire
+*/
 export function switchFaction() {
   return {
     type: SWITCH_FACTION
   };
 }
 
+/*
+  Associated wtih the FilterToggle component
+  Changes whether a category of entities (rune wards, push wards, etc) should be shown
+*/
 export function toggleFilter(group, filter) {
   return {
     type: TOGGLE_FILTER,
@@ -16,6 +24,10 @@ export function toggleFilter(group, filter) {
   };
 }
 
+/*
+  Associated wtih the ControlLabel component
+  Changes whether an entire group of filters (wards, camps, etc) should be shown
+*/
 export function toggleGroup(group, on) {
   return {
     type: TOGGLE_GROUP,
