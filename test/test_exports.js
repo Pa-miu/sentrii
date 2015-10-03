d625487f01d61fd61f629722189ec4312b4e73ab
kusera <kuserasera@gmail.com>
2015-10-03 02:22:39 -0400

    Add rudimentary mocha tests

---
+++ test/test_exports.js
@@ -0,0 +1,26 @@
+import React from 'react/addons';
+import chai from 'chai';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+
+let { assert, expect } = chai,
+    { TestUtils } = React.addons;
+
+chai.should();
+chai.use(sinonChai);
+
+export {
+  React,
+  chai,
+  sinon,
+  sinonChai,
+  assert,
+  expect,
+  TestUtils
+}
+
+/*
+ Add the following code to import necessary modules to test specs
+
+  import { React, sinon, assert, expect, TestUtils } from '../../test_helper';
+*/
