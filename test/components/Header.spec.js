2be74ac43ed25cf7f69044c08b7ece639b8bb023
kusera <kuserasera@gmail.com>
2015-10-03 18:55:58 -0400

    Add basic component testing for Header

---
+++ test/components/Header.spec.js
@@ -0,0 +1,27 @@
+import { React, sinon, assert, expect, TestUtils } from '../test_exports';
+import Header from '../../src/js/components/Header';
+
+describe('Header component', () => {
+  let sandbox, header, container, titleGroup, title, subtitle, navbar, navItems;
+  let findDOMWithClass = TestUtils.findRenderedDOMComponentWithClass;
+  let scryDOMWithClass = TestUtils.scryRenderedDOMComponentsWithClass;
+
+  before(() => {
+    header = TestUtils.renderIntoDocument(<Header/>);
+    container = findDOMWithClass(header, 'header-container');
+    titleGroup = findDOMWithClass(header, 'title-group');
+    title = findDOMWithClass(header, 'title');
+    subtitle = findDOMWithClass(header, 'subtitle');
+    navbar = findDOMWithClass(header, 'navbar');
+    navItems = scryDOMWithClass(header, 'nav-item');
+  });
+
+  it('should render into the DOM', () => {
+    expect(container).to.not.equal(null);
+    expect(titleGroup).to.not.equal(null);
+    expect(title).to.not.equal(null);
+    expect(subtitle).to.not.equal(null);
+    expect(navbar).to.not.equal(null);
+    expect(navItems.length).to.equal(3);
+  });
+});
